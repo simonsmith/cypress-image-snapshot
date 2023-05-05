d2dad1dd2e8ed951b36835b1b66484fe69551e76
Simon Smith <simonsmith@users.noreply.github.com>
2023-05-05 16:28:52 +0100

    test: add http-server for test page

--- cypress/e2e/matchImageSnapshot.cy.ts
+++ cypress/e2e/matchImageSnapshot.cy.ts
@@ -1,16 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io').matchImageSnapshot()
-    cy.matchImageSnapshot({
-      jestImageSnapshotOptions: {
-        failureThreshold: 100,
-      },
-    })
-    cy.matchImageSnapshot('spec name')
-    cy.matchImageSnapshot('filename', {
-      jestImageSnapshotOptions: {
-        failureThreshold: 1,
-      },
-    })
-  })
+before(() => {
+  cy.visit('http://localhost:9001')
+})
+
+it('matches snapshot with defaults', () => {
+  cy.matchImageSnapshot()
 })
