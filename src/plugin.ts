simonsmith/cypress-image-snapshot

commit ef49519795daf5183f4fac6f3136e194f20f39f4

fix: sanitise snapshot filenames
Prevents potential for tests to create directories/images outside of the repository
root. Makes use of the Cypress screenshot path which has already been
sanitised

Fixes #15

src/plugin.ts
@@ -55,8 +55,7 @@ const runImageDiffAfterScreenshot = async (
     return {path: screenshotPath}
   }
 
-  // name of the screenshot without the Cypress suffixes for test failures
-  const snapshotName = screenshotConfig.name.replace(/ \(attempt [0-9]+\)/, '')
+  const snapshotName = path.basename(screenshotConfig.path, '.png')
 
   const receivedImageBuffer = await fs.readFile(screenshotPath)
   await fs.rm(screenshotPath)

cypress/e2e/matchImageSnapshot.cy.ts
@@ -11,6 +11,11 @@ it('name and selector', () => {
   cy.get('body').matchImageSnapshot('with custom name')
 })
 
+it('file name should ignore directories', () => {
+  cy.get('h1').matchImageSnapshot('../../../ignore-relative-dirs')
+  cy.get('h1').matchImageSnapshot('ignore/folders/image')
+})
+
 // next two tests use blackout to change
 // the snapshot image. Also validates options
 it('name and options', () => {

cypress/e2e/nested/test/matchImageSnapshot.cy.ts
@@ -6,3 +6,7 @@ beforeEach(() => {
 it('takes a snapshot of the page', () => {
   cy.matchImageSnapshot()
 })
+
+it('file name should ignore directories', () => {
+  cy.get('h1').matchImageSnapshot('../../../ignore-relative-dirs')
+})

commit 419167d2343810cb3f94108d46080c514552fe03

fix: remove attempt suffix from snapshot name
A regression created in
ef49519795daf5183f4fac6f3136e194f20f39f4

Fixes #19

src/plugin.ts
@@ -78,7 +78,10 @@ const runImageDiffAfterScreenshot = async (
       // In open mode it's an empty string so is ignored
       .replace(screenshotsFolder, ''),
   )
-  snapshotName = path.join(dirName, snapshotName)
+  snapshotName = path
+    .join(dirName, snapshotName)
+    .replace(/ \(attempt [0-9]+\)/, '')
+
   log('snapshotName', snapshotName)
   log('screenshotConfig', screenshotConfig)
 